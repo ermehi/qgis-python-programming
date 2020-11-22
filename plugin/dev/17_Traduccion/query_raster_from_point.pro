17dbc2af6bee6f3cad2f925d7caa3d7db241ca58
dortegat <dortegat@gmail.com>
2020-11-22 14:17:30 +0100

    plugins

---
+++ plugin/dev/17_Traduccion/query_raster_from_point.pro
@@ -0,0 +1,6 @@
+FORMS = ../query_raster_from_point_dialog_base.ui
+
+SOURCES = ../query_raster_from_point.py \
+../query_raster_from_point_dialog.py
+
+TRANSLATIONS = query_raster_from_point_es.ts
---
+++ plugin/dev/17_Traduccion/query_raster_from_point_es.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>QueryRasterFromPoint</name>
+    <message>
+        <location filename="../query_raster_from_point.py" line="158"/>
+        <source>&amp;Query raster from point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="148"/>
+        <source>Query raster values from point vector layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="192"/>
+        <source>Select vector layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="200"/>
+        <source>Select raster layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="210"/>
+        <source>Select path output shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="236"/>
+        <source>Vector layer and raster layer have different CRSs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="300"/>
+        <source>--- Select vector layer ---</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="311"/>
+        <source>--- Select raster layer ---</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="320"/>
+        <source>Open shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="333"/>
+        <source>Open raster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="349"/>
+        <source>Processing completed successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point.py" line="372"/>
+        <source>Search path output shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QueryRasterFromPointDialogBase</name>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="14"/>
+        <source>Query raster from point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="20"/>
+        <source>Input data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="28"/>
+        <source>Vector layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="189"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="58"/>
+        <source>Raster layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="68"/>
+        <source>Raster layers loaded in the QGIS Table Of Contents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="97"/>
+        <source>Output layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="105"/>
+        <source>Memory layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="112"/>
+        <source>Remove previous processings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="139"/>
+        <source>Shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../query_raster_from_point_dialog_base.ui" line="149"/>
+        <source>Search path output shapefile...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
